c3c2ee1ee3cb34f3fe5cf5646ac7b4e388739ea5
Nasina7 <38535607+Nasina7@users.noreply.github.com>
2025-07-01 22:48:04 -0400

    Link byte handling

--- app/components/mappings/raw-editor.js
+++ app/components/mappings/raw-editor.js
@@ -1,335 +1,324 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    SortableContainer,
-    SortableElement,
-    SortableHandle,
-} from 'react-sortable-hoc';
-import arrayMove from 'array-move';
-import { mappingState } from './state';
-import { environment } from '#store/environment';
-import { observer } from 'mobx-react';
-import { Select, Input, Item, Button, Modal } from '#ui';
-import { Tile } from '../art/tile';
+  SortableContainer,
+  SortableElement,
+  SortableHandle,
+} from "react-sortable-hoc";
+import arrayMove from "array-move";
+import { mappingState } from "./state";
+import { environment } from "#store/environment";
+import { observer } from "mobx-react";
+import { Select, Input, Item, Button, Modal } from "#ui";
+import { Tile } from "../art/tile";
 import {
-    isNumber,
-    isPositiveNumber,
-    isDPLCSize,
-    isWidthHeight,
-} from '#util/assertions';
+  isNumber,
+  isPositiveNumber,
+  isDPLCSize,
+  isWidthHeight,
+} from "#util/assertions";
 
 const Handle = SortableHandle(() => (
-    <div className="handle">
-        <svg viewBox="20 20 56 56" width="26" height="26">
-            <path d="M73 48.4l-10.4-9.6v4.8H52.4V33.4h4.8L47.6 23l-9 10.4h5v10.2H33.3v-4.8L23 48.4l10.4 9v-5h10.2v10.3h-4.8L47.6 73l9.6-10.4h-4.8V52.4h10.2v4.8L73 48.4z" />
-        </svg>
-    </div>
+  <div className="handle">
+    <svg viewBox="20 20 56 56" width="26" height="26">
+      <path d="M73 48.4l-10.4-9.6v4.8H52.4V33.4h4.8L47.6 23l-9 10.4h5v10.2H33.3v-4.8L23 48.4l10.4 9v-5h10.2v10.3h-4.8L47.6 73l9.6-10.4h-4.8V52.4h10.2v4.8L73 48.4z" />
+    </svg>
+  </div>
 ));
 
 // mappings
 
 const SortableMappingItem = SortableElement(
-    observer(({ mapping, mappingIndex }) => (
-        <div className="mapping-data">
-            <Handle />
-            <div className="properties">
-                <div className="datum">
-                    <div className="label">top</div>
-                    <Input
-                        store={mapping}
-                        accessor="top"
-                        assert={isNumber}
-                        isNumber
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">left</div>
-                    <Input
-                        store={mapping}
-                        accessor="left"
-                        assert={isNumber}
-                        isNumber
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">tile</div>
-                    <Input
-                        store={mapping}
-                        accessor="art"
-                        assert={isPositiveNumber}
-                        isNumber
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">width</div>
-                    <Input
-                        store={mapping}
-                        accessor="width"
-                        assert={isWidthHeight}
-                        isNumber
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">height</div>
-                    <Input
-                        store={mapping}
-                        accessor="height"
-                        assert={isWidthHeight}
-                        isNumber
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">priority</div>
-                    <Select
-                        store={mapping}
-                        accessor="priority"
-                        options={[
-                            { label: 'yes', value: true },
-                            { label: 'no', value: false },
-                        ]}
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">vflip</div>
-                    <Select
-                        store={mapping}
-                        accessor="vflip"
-                        options={[
-                            { label: 'yes', value: true },
-                            { label: 'no', value: false },
-                        ]}
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">hflip</div>
-                    <Select
-                        store={mapping}
-                        accessor="hflip"
-                        options={[
-                            { label: 'yes', value: true },
-                            { label: 'no', value: false },
-                        ]}
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">palette</div>
-                    <Select
-                        store={mapping}
-                        accessor="palette"
-                        options={[0, 1, 2, 3]}
-                        flipScroll
-                    />
-                </div>
-            </div>
-            <Item
-                inverted
-                onClick={() => {
-                    environment.currentSprite.mappings.splice(mappingIndex, 1);
-                }}
-            >
-                Delete
-            </Item>
+  observer(({ mapping, mappingIndex }) => (
+    <div className="mapping-data">
+      <Handle />
+      <div className="properties">
+        <div className="datum">
+          <div className="label">top</div>
+          <Input store={mapping} accessor="top" assert={isNumber} isNumber />
         </div>
-    )),
+        <div className="datum">
+          <div className="label">left</div>
+          <Input store={mapping} accessor="left" assert={isNumber} isNumber />
+        </div>
+        <div className="datum">
+          <div className="label">tile</div>
+          <Input
+            store={mapping}
+            accessor="art"
+            assert={isPositiveNumber}
+            isNumber
+          />
+        </div>
+        <div className="datum">
+          <div className="label">width</div>
+          <Input
+            store={mapping}
+            accessor="width"
+            assert={isWidthHeight}
+            isNumber
+          />
+        </div>
+        <div className="datum">
+          <div className="label">height</div>
+          <Input
+            store={mapping}
+            accessor="height"
+            assert={isWidthHeight}
+            isNumber
+          />
+        </div>
+        <div className="datum">
+          <div className="label">priority</div>
+          <Select
+            store={mapping}
+            accessor="priority"
+            options={[
+              { label: "yes", value: true },
+              { label: "no", value: false },
+            ]}
+          />
+        </div>
+        <div className="datum">
+          <div className="label">vflip</div>
+          <Select
+            store={mapping}
+            accessor="vflip"
+            options={[
+              { label: "yes", value: true },
+              { label: "no", value: false },
+            ]}
+          />
+        </div>
+        <div className="datum">
+          <div className="label">hflip</div>
+          <Select
+            store={mapping}
+            accessor="hflip"
+            options={[
+              { label: "yes", value: true },
+              { label: "no", value: false },
+            ]}
+          />
+        </div>
+        <div className="datum">
+          <div className="label">palette</div>
+          <Select
+            store={mapping}
+            accessor="palette"
+            options={[0, 1, 2, 3]}
+            flipScroll
+          />
+        </div>
+        <div className="datum">
+          <div className="label">link</div>
+          <Select
+            store={mapping}
+            accessor="link"
+            options={[0, 1, 2, 3]}
+            flipScroll
+          />
+        </div>
+      </div>
+      <Item
+        inverted
+        onClick={() => {
+          environment.currentSprite.mappings.splice(mappingIndex, 1);
+        }}
+      >
+        Delete
+      </Item>
+    </div>
+  )),
 );
 
 const SortableMappingList = SortableContainer(
-    observer(
-        class extends Component {
-            render() {
-                const { items } = this.props;
-                return (
-                    <div>
-                        {items.map((mapping, index) => (
-                            <SortableMappingItem
-                                key={`item-${index}`}
-                                index={index}
-                                mapping={mapping}
-                                mappingIndex={index}
-                            />
-                        ))}
-                    </div>
-                );
-            }
-        },
-    ),
-    { withRef: true },
+  observer(
+    class extends Component {
+      render() {
+        const { items } = this.props;
+        return (
+          <div>
+            {items.map((mapping, index) => (
+              <SortableMappingItem
+                key={`item-${index}`}
+                index={index}
+                mapping={mapping}
+                mappingIndex={index}
+              />
+            ))}
+          </div>
+        );
+      }
+    },
+  ),
+  { withRef: true },
 );
 
 // dplcs
 
 const SortableDPLCItem = SortableElement(
-    observer(({ dplc, dplcIndex }) => (
-        <div className="mapping-data">
-            <Handle />
-            <div className="properties">
-                <div className="datum">
-                    <div className="label">tile</div>
-                    <Input
-                        store={dplc}
-                        accessor="art"
-                        assert={isPositiveNumber}
-                        isNumber
-                    />
-                </div>
-                <div className="datum">
-                    <div className="label">length</div>
-                    <Input
-                        store={dplc}
-                        accessor="size"
-                        assert={isDPLCSize}
-                        isNumber
-                    />
-                </div>
-                <div className="dplc-tiles">
-                    {Array.from({ length: dplc.size }).map((_, i) => {
-                        const data =
-                            environment.tiles.length > dplc.art + i
-                                ? environment.tiles[dplc.art + i]
-                                : undefined;
-                        return <Tile key={i} data={data} />;
-                    })}
-                </div>
-            </div>
-            <Item
-                inverted
-                onClick={() => {
-                    environment.currentSprite.dplcs.splice(dplcIndex, 1);
-                }}
-            >
-                Delete
-            </Item>
+  observer(({ dplc, dplcIndex }) => (
+    <div className="mapping-data">
+      <Handle />
+      <div className="properties">
+        <div className="datum">
+          <div className="label">tile</div>
+          <Input
+            store={dplc}
+            accessor="art"
+            assert={isPositiveNumber}
+            isNumber
+          />
         </div>
-    )),
+        <div className="datum">
+          <div className="label">length</div>
+          <Input store={dplc} accessor="size" assert={isDPLCSize} isNumber />
+        </div>
+        <div className="dplc-tiles">
+          {Array.from({ length: dplc.size }).map((_, i) => {
+            const data =
+              environment.tiles.length > dplc.art + i
+                ? environment.tiles[dplc.art + i]
+                : undefined;
+            return <Tile key={i} data={data} />;
+          })}
+        </div>
+      </div>
+      <Item
+        inverted
+        onClick={() => {
+          environment.currentSprite.dplcs.splice(dplcIndex, 1);
+        }}
+      >
+        Delete
+      </Item>
+    </div>
+  )),
 );
 
 const SortableDPLCList = SortableContainer(
-    observer(
-        class extends Component {
-            render() {
-                const { items } = this.props;
-                return (
-                    <div>
-                        {items.map((dplc, index) => (
-                            <SortableDPLCItem
-                                key={`item-${index}`}
-                                index={index}
-                                dplc={dplc}
-                                dplcIndex={index}
-                            />
-                        ))}
-                    </div>
-                );
-            }
-        },
-    ),
-    { withRef: true },
+  observer(
+    class extends Component {
+      render() {
+        const { items } = this.props;
+        return (
+          <div>
+            {items.map((dplc, index) => (
+              <SortableDPLCItem
+                key={`item-${index}`}
+                index={index}
+                dplc={dplc}
+                dplcIndex={index}
+              />
+            ))}
+          </div>
+        );
+      }
+    },
+  ),
+  { withRef: true },
 );
 
 export const RawEditor = observer(
-    class RawEditor extends Component {
-        onSortEndMapping = ({ oldIndex, newIndex }) => {
-            const {
-                currentSprite: { mappings },
-            } = environment;
-            mappings.replace(arrayMove(mappings, oldIndex, newIndex));
-        };
+  class RawEditor extends Component {
+    onSortEndMapping = ({ oldIndex, newIndex }) => {
+      const {
+        currentSprite: { mappings },
+      } = environment;
+      mappings.replace(arrayMove(mappings, oldIndex, newIndex));
+    };
 
-        addNewMapping = () => {
-            const {
-                currentSprite: { mappings },
-            } = environment;
-            mappings.push({
-                art: 0,
-                priority: false,
-                left: 0,
-                top: 0,
-                width: 1,
-                height: 1,
-                vflip: false,
-                hflip: false,
-                palette: 0,
-            });
-        };
+    addNewMapping = () => {
+      const {
+        currentSprite: { mappings },
+      } = environment;
+      mappings.push({
+        art: 0,
+        priority: false,
+        left: 0,
+        top: 0,
+        width: 1,
+        height: 1,
+        vflip: false,
+        hflip: false,
+        palette: 0,
+        link: 0,
+      });
+    };
 
-        onSortEndDPLC = ({ oldIndex, newIndex }) => {
-            const {
-                currentSprite: { dplcs },
-            } = environment;
-            dplcs.replace(arrayMove(dplcs, oldIndex, newIndex));
-        };
+    onSortEndDPLC = ({ oldIndex, newIndex }) => {
+      const {
+        currentSprite: { dplcs },
+      } = environment;
+      dplcs.replace(arrayMove(dplcs, oldIndex, newIndex));
+    };
 
-        addNewDPLC = () => {
-            const {
-                currentSprite: { dplcs },
-            } = environment;
-            dplcs.push({
-                art: 0,
-                size: 1,
-            });
-        };
+    addNewDPLC = () => {
+      const {
+        currentSprite: { dplcs },
+      } = environment;
+      dplcs.push({
+        art: 0,
+        size: 1,
+      });
+    };
 
-        render() {
-            const {
-                currentSprite: { mappings, dplcs },
-                config: { currentTile, dplcsEnabled },
-            } = environment;
-            const {
-                rawEditor: { active },
-            } = mappingState;
+    render() {
+      const {
+        currentSprite: { mappings, dplcs },
+        config: { currentTile, dplcsEnabled },
+      } = environment;
+      const {
+        rawEditor: { active },
+      } = mappingState;
 
-            return (
-                <Modal
-                    spring={{
-                        top: active ? 15 : -100,
-                        opacity: active ? 1 : 0,
-                    }}
-                    className="raw-mapping-data"
-                >
-                    <div className="close">
-                        <Item>Raw Mapping Editor</Item>
-                        <Button
-                            color="magenta"
-                            onClick={mappingState.toggleRawEditor}
-                        >
-                            Close
-                        </Button>
-                    </div>
-                    <SortableMappingList
-                        axis="y"
-                        lockAxis="y"
-                        items={mappings}
-                        lockToContainerEdges={true}
-                        useDragHandle={true}
-                        onSortEnd={this.onSortEndMapping}
-                    />
-                    <Item onClick={this.addNewMapping} inverted>
-                        Add New Mapping
-                    </Item>
-                    {dplcsEnabled && (
-                        <div className="raw-dplc-data">
-                            <SortableDPLCList
-                                axis="y"
-                                lockAxis="y"
-                                items={dplcs || []}
-                                lockToContainerEdges={true}
-                                useDragHandle={true}
-                                onSortEnd={this.onSortEndDPLC}
-                            />
-                            <div className="buttons">
-                                <Item onClick={this.addNewDPLC} inverted>
-                                    Add New DPLC
-                                </Item>
-                                <Item
-                                    onClick={mappingState.optimizeCurrentDPLCs}
-                                    inverted
-                                >
-                                    Optimise DPLCs
-                                </Item>
-                            </div>
-                        </div>
-                    )}
-                </Modal>
-            );
-        }
-    },
+      return (
+        <Modal
+          spring={{
+            top: active ? 15 : -100,
+            opacity: active ? 1 : 0,
+          }}
+          className="raw-mapping-data"
+        >
+          <div className="close">
+            <Item>Raw Mapping Editor</Item>
+            <Button color="magenta" onClick={mappingState.toggleRawEditor}>
+              Close
+            </Button>
+          </div>
+          <SortableMappingList
+            axis="y"
+            lockAxis="y"
+            items={mappings}
+            lockToContainerEdges={true}
+            useDragHandle={true}
+            onSortEnd={this.onSortEndMapping}
+          />
+          <Item onClick={this.addNewMapping} inverted>
+            Add New Mapping
+          </Item>
+          {dplcsEnabled && (
+            <div className="raw-dplc-data">
+              <SortableDPLCList
+                axis="y"
+                lockAxis="y"
+                items={dplcs || []}
+                lockToContainerEdges={true}
+                useDragHandle={true}
+                onSortEnd={this.onSortEndDPLC}
+              />
+              <div className="buttons">
+                <Item onClick={this.addNewDPLC} inverted>
+                  Add New DPLC
+                </Item>
+                <Item onClick={mappingState.optimizeCurrentDPLCs} inverted>
+                  Optimise DPLCs
+                </Item>
+              </div>
+            </div>
+          )}
+        </Modal>
+      );
+    }
+  },
 );
---
+++ scripts/Puyo.js
@@ -0,0 +1,105 @@
+// Flex 2 Mapping Definition - Puyo Puyo / Mean Bean Machine
+
+// Set VRAM tile offset here
+// (For example, art/cutscene/general/arle.puyo is loaded starting at VRAM tile 0x300)
+const offset = 0x0;
+
+const {
+  mappings,
+  dplcs,
+  offsetTable,
+  write,
+  read,
+  dc,
+  nybble,
+  endFrame,
+  skipFrame,
+  signed,
+  asm,
+} = Flex2;
+
+mappings([
+  offsetTable(dc.l),
+  [
+    () => {
+      const quantity = read(dc.w);
+      return (
+        quantity > 0 &&
+        (({ mapping }, frameIndex) => {
+          mapping.top = read(dc.w, signed);
+          read(nybble);
+          mapping.width = read(2) + 1;
+          mapping.height = read(2) + 1;
+          mapping.link = read(dc.b);
+          mapping.priority = read(1);
+          mapping.palette = read(2);
+          mapping.vflip = read(1);
+          mapping.hflip = read(1);
+          mapping.art = read(11) - offset;
+          mapping.left = read(dc.w, signed);
+          if (frameIndex === quantity - 1) return endFrame;
+        })
+      );
+    },
+    ({ sprite }) => {
+      write(dc.w, sprite.length);
+      return ({ mapping }) => {
+        write(dc.w, mapping.top);
+        write(nybble, 0);
+        write(2, mapping.width - 1);
+        write(2, mapping.height - 1);
+        write(dc.b, mapping.link);
+        write(1, mapping.priority);
+        write(2, mapping.palette);
+        write(1, mapping.vflip);
+        write(1, mapping.hflip);
+        write(11, mapping.art + offset);
+        write(dc.w, mapping.left);
+      };
+    },
+  ],
+]);
+
+asm(({ addScript, importScript, writeMappings, writeDPLCs }) => {
+  importScript("MapMacros.asm");
+
+  writeMappings(({ label, sprites, renderHex }) => {
+    const list = [];
+
+    list.push(`${label}: mappingsTable`);
+    sprites.forEach((_, i) => {
+      list.push(`\tmappingsTableEntry.l\t${label}_${i}`);
+    });
+    list.push("");
+
+    sprites.forEach((sprite, i) => {
+      list.push(`${label}_${i}:\tspriteHeader`);
+
+      sprite.mappings.forEach((mapping) => {
+        const pieceInfo = [
+          mapping.left,
+          mapping.top,
+          mapping.width,
+          mapping.height,
+          mapping.art + offset,
+          mapping.hflip,
+          mapping.vflip,
+          mapping.palette,
+          mapping.priority,
+          mapping.link,
+        ]
+          .map(renderHex)
+          .join(", ");
+
+        list.push(` spritePiece ${pieceInfo}`);
+      });
+
+      list.push(`${label}_${i}_End`);
+      list.push("");
+    });
+
+    list.push("\teven");
+
+    return list.join("\n");
+  });
+});
